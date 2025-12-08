8cc892e76ed11f002fb16235b58ccd4d6687166b
Robert  Wilson <robertwilson190@gmail.com>
2025-12-08 17:41:36 +0000

    add test of use of cache

--- tests/test_matchup_formula.py
+++ tests/test_matchup_formula.py
@@ -16,6 +16,44 @@ class TestFinal:
 
         oceanval.definitions.reset()
 
+
+        oceanval.add_point_comparison(
+            name = "chlorophyll",
+            obs_path="data/chl/obs_csv",
+            source = "foo",
+            model_variable = "P1_Chl+P2_Chl+P3_Chl+P4_Chl",
+            start = 2000, 
+            end = 2000
+        )
+
+        oceanval.matchup(
+            "data/chl/simulation",
+            start = 2000, 
+            end = 2000, 
+            n_dirs_down = 0,
+            ask = False,
+            cache = True,
+            cores = 1,
+            require = "model", 
+            exclude = "bad"
+        )
+
+        ff = "oceanval_matchups/point/surface/chlorophyll/foo/foo_surface_chlorophyll.csv"
+        df = pd.read_csv(ff)
+        diff = np.abs(df['model'] - df['observation'])
+        max_diff = diff.max()
+        assert max_diff < 1e-6
+
+        for ff in glob.glob(".cache_oceanval/output/*.pkl"):
+            with open(ff, "rb") as f:
+                df = pickle.load(f)   
+
+                columns = ["lon", "lat", "year", "month", "day", "total"]
+                for col in columns:
+                    assert col in df.columns
+                assert len(df) == 248
+
+
         oceanval.add_gridded_comparison(
             name = "chlorophyll",
             obs_path="data/chl/model_as_obs",
@@ -23,15 +61,6 @@ class TestFinal:
             model_variable = "P1_Chl+P2_Chl+P3_Chl+P4_Chl",
             obs_variable = "total",
             climatology = False,
-            start = 2000, 
-            end = 2000
-        )
-
-        oceanval.add_point_comparison(
-            name = "chlorophyll",
-            obs_path="data/chl/obs_csv",
-            source = "foo",
-            model_variable = "P1_Chl+P2_Chl+P3_Chl+P4_Chl",
             start = 2000, 
             end = 2000
         )
