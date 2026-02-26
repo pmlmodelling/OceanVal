7069c9892421de0f75e0c087232c1f2291b0e26c
Robert  Wilson <robertwilson190@gmail.com>
2026-02-26 08:58:26 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ extras_require: dict() = dict()
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(name='oceanval',
-      version='0.1.9',
+      version='0.2.0',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       python_requires='>=3.6.1',
